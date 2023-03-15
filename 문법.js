1b1ad586b5eaec230593605ade01ed7737554369
JWP2023 <garook@e-mirim.hs.kr>
2023-03-15 09:40:38 +0900

    const

--- 문법.js
+++ 문법.js
@@ -2,3 +2,10 @@
   JavaScript에서 false로 처리되는 값들
   null, NaN, 0, 빈 문자열("", '', ``), undefined, false
 */
+
+
+// a를 상수처리
+const a = 30;
+console.log(a);   // 30
+// a = 10;           // 에러 const는 값을 바꿀 수 없다.
+
