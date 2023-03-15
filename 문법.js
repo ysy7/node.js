5f37408c1a3234a6f036aa188689103d7706a81e
JWP2023 <garook@e-mirim.hs.kr>
2023-03-15 09:53:22 +0900

    함수 기본 사용법

--- 문법.js
+++ 문법.js
@@ -5,7 +5,25 @@
 
 
 // a를 상수처리
-const a = 30;
-console.log(a);   // 30
-// a = 10;           // 에러 const는 값을 바꿀 수 없다.
+// const a = 30;
+// console.log(a);   // 30
+// // a = 10;           // 에러 const는 값을 바꿀 수 없다.
 
+
+// 함수
+function add(n1, n2) {
+  return n1+n2;
+}
+
+result = add(1, 2);
+console.log(result)   // 3
+
+plus = add;           // plus도 add()의 역할을 할 수 있음
+result = plus(3, 4);
+console.log(result);  // 7
+
+sub = function(n1, n2) {
+  return n1-n2;
+}
+result = sub(3, 2);
+console.log(result)   // 1
