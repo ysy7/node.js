ae678cc3341e9c64904b31338b2944fa5a3b2f2d
ysy7 <w2105@e-mirim.hs.kr>
2023-03-22 09:06:25 +0900

    ==, ===비교하기

---
+++ 동등비교.js
@@ -0,0 +1,12 @@
+//동등연산자 (abstract equility)
+console.log(1 == '1');
+
+//일치연산자 (strict equility)
+console.log(1 === '1');
+
+console.log('hello' == 'hello');
+console.log('hello' === 'hello');
+
+console.log(0 == false);
+console.log(0 === false);
+
