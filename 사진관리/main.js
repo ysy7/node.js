e3175f0d4b974a56a4401d9acb798c203b7db49b
ysy7 <w2105@e-mirim.hs.kr>
2023-04-13 15:11:25 +0900

    중복된 파일 찾기 getDuplicated()

--- 사진관리/main.js
+++ 사진관리/main.js
@@ -1,44 +1,64 @@
-const { dir } = require("console");
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-//dirPath에 있는 모든 파일목록의 배열(하위경로까지)
+// dirPath에 있는 모든 파일목록의 배열(하위 경로까지)
 function getAllFiles(dirPath, arrOfFiles) {
-  //dirPath에 있는 모든 파일목록의 배열(하위 경로는 불가)
+  // dirPath에 있는 모든 파일 목록의 배열(하위 경로는 불가)
   const files = fs.readdirSync(dirPath);
 
-  files.forEach(function (file) {
-    const destPath = path.join(dirPath, file);
-    //dirPath + "\\" + file;
-    //폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다
-    if (fs.statSync(dirPath + "\\" + file).isDirectory()) {
+  files.forEach((file) => {
+    const destPath = path.join(dirPath,file);
+
+    if(fs.statSync(destPath).isDirectory()) {
+      // 디렉토리라면 해당 폴더에 있는 모든 파일목록들을 체크
       getAllFiles(destPath, arrOfFiles);
     } else {
-      //파일이라면 파일목록에 추가
+      // 파일이면 파일 목록에 추가
       arrOfFiles.push(destPath);
     }
   });
+
   return arrOfFiles;
 }
 
-function getDuplicated(baseDir) {
-  if (!fs.existsSync(baseDir)) {
-    console.log("폴더가 존재하지 않음");
+function getDuplicated(baseDir){
+
+  //해당 디렉토리가 존재하는지 유효성 검사
+  if(!fs.existsSync(baseDir)) {
+    console.log('폴더가 존재하지 않음');
     return;
-  } 
-  
-  const duplicatedDir = path.join(baseDir,'duplicated');
+  }
+  const duplicatedDir = path.join(baseDir, 'duplicated');
   if(!fs.existsSync(duplicatedDir))
-  fs.mkdirSync(duplicatedDir);
+      fs.mkdirSync(duplicatedDir);
 
-  //base폴더에 있는 모든 파일들 list
-  const arrayOfFiles = getAllFiles(baseDir,[] );
+  const arrayOfFiles = getAllFiles(baseDir, []);
 
-  //중복된 파일들 (절대경로) list
   const duplicatedFiles = [];
-}
-  
-// const files = getAllFiles(path.join(__dirname, 'base'), []);
-// console.log(files.join("\n"));
 
-getDuplicated(path.join(__dirname, "base"));
+  arrayOfFiles.forEach(function(file, idx) {
+    const fileName = path.basename(file);
+
+  const duplicatedIdx = arrayOfFiles.findIndex(function(otherFile, otherIdx) {
+    if(otherIdx > idx && otherFile.includes(fileName))
+      return true;
+    else 
+      return false;
+  })
+
+  if(duplicatedIdx > -1){
+    const duplicatedFile = arrayOfFiles[duplicatedIdx];
+    duplicatedFiles.push(file);
+    duplicatedFiles.push(duplicatedFile);
+  }
+
+});
+return duplicatedFiles
+}
+const files = getDuplicated(path.join(dirname, 'base'));
+console.log(files.join('\n'));
+
+// const files = getAllFiles(dirname + '\base', []);
+// console.log(files)
+
+
