d596677045711b041917a427152240504196aa88
JWP2023 <garook@e-mirim.hs.kr>
2023-04-19 10:12:47 +0900

    파일이름 바꾸기

--- 문법/fs.js
+++ 문법/fs.js
@@ -16,9 +16,13 @@ console.log(result);
 console.log(path.basename(result));   // path.basename()파일명만 추출
 
 
-const copiedFile = path.join(__dirname, '복사한파일.js')
+const copiedFile = path.join(__dirname, '복사한파일.js');
 fs.copyFileSync(result, copiedFile);  // 파일 복사
-fs.unlinkSync(copiedFile);            // 파일 삭제
+
+const renamedFile = path.join(__dirname, '변경한파일.js');
+fs.renameSync(copiedFile, renamedFile); // 파일 이름 바꾸기
+
+//fs.unlinkSync(copiedFile);            // 파일 삭제
 
 // 해당 파일에 대한 정보를 알 수 있음
 console.log(fs.statSync(result));
