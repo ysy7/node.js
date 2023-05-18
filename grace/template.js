87e946c0af63bd48174cc635946995955b3f4b31
ysy7 <w2105@e-mirim.hs.kr>
2023-05-18 14:57:23 +0900

    food 완성

--- grace/template.js
+++ grace/template.js
@@ -5,9 +5,14 @@ const ejs = require('ejs');
 
 // index.ejs에 넘겨줄 data
 const index = {
-  name: 'JWP',
+  name: '서양',
   here: '한국'
 };
+
+const food1 = "쿠키";
+const food2 = "초밥";
+const food3 = "타이거새우";
+
 
 const server = http.createServer(function(req, res) {
   console.log(req.url);
@@ -22,6 +27,9 @@ const server = http.createServer(function(req, res) {
     
   }
   else if(req.url === '/food') {
+    ejs.renderFile(path.join(__dirname,'template', 'food.ejs'),
+    {food1: food1, food2: food2, food3: food3})
+    .then((data) => res.end(data))
     fs.createReadStream(path.join(__dirname, 'html', 'food.html')).pipe(res);
   }
   else {
