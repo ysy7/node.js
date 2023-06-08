1cc467feb020f1d2eea17dd468022ac705d2aa41
ysy7 <w2105@e-mirim.hs.kr>
2023-06-08 13:37:30 +0900

    mysql2

---
+++ mysql/mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+const path = require('path');
+
+require('dotenv').config({
+  path: path.join(__dirname, '.env')
+});
+
+// mysql 시스템 정보(절대로 공개하면 안됨)
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+});
+
+pool.query("SELECT * FROM player", (err, results) => {
+  for(const r of results) {
+    console.log(r);
+  }
+});
